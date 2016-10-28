2242dfe305c2469d1cf04d563333d7eaac3f16a0
Francesco Negri <francesco.negri.98@gmail.com>
2016-10-28 19:17:58 +0200

    Added home and jsconfig.json for VS Code

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+app.get('/home', function(req, res) {
+    res.send('Benvenuto nella mia home!');
+});
+
 app.listen(3000, function(){
     console.log('Test app running on port 3000!');
 });
+
+
